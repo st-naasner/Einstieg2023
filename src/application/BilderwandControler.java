81603c8a62952390cdd1fcaff06a1ec3ca760b85
st-naasner <st.naasner@web.de>
2023-01-10 00:42:05 +0100

    Projekt funktionsfähig

---
+++ src/application/BilderwandControler.java
@@ -0,0 +1,21 @@
+package application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class BilderwandControler {
+
+    @FXML
+    private Button btNaasner;
+
+    @FXML
+    private ImageView imBild;
+
+    @FXML
+    void anzeigenNaasner(ActionEvent event) {
+    	
+    }
+
+}
