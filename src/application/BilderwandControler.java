d9fc831f1188b9bde891ee09d66ce8c0bd45a51d
st-naasner <st.naasner@web.de>
2023-01-10 22:29:50 +0100

    Als Vorlage für den Kurs

--- src/application/BilderwandControler.java
+++ src/application/BilderwandControler.java
@@ -7,8 +7,45 @@ import javafx.scene.image.ImageView;
 
 public class BilderwandControler {
 
+
+    @FXML
+    private Button btBatu;
+
+    @FXML
+    private Button btBoris;
+
+    @FXML
+    private Button btDaniel;
+
+    @FXML
+    private Button btJustin;
+
+    @FXML
+    private Button btKilian;
+
+    @FXML
+    private Button btLinas;
+
+    @FXML
+    private Button btMagnus;
+
+    @FXML
+    private Button btMohanad;
+
     @FXML
     private Button btNaasner;
+
+    @FXML
+    private Button btNiclas;
+
+    @FXML
+    private Button btNicolas;
+
+    @FXML
+    private Button btTim;
+
+    @FXML
+    private Button btfarid;
 
     @FXML
     private ImageView imBild;
